63f79ecfd57aaba7e5f5960e3e0392acf1d8bcc6
APDevice <59993100+APDevice@users.noreply.github.com>
2020-06-07 20:49:40 -0700

    Update nrf52840.py

--- nrf52840.py
+++ nrf52840.py
@@ -6,7 +6,8 @@ class builtin_led:
     """
     Controls the builtin RGB LED
     
-    .set_rgb(red, green, blue) / values must be between 0 - 255
+    .set_rgb(red, green, blue)
+    Values must be between 0 - 255
     """
     def __init__(self):
         self.pwm_led_red = pulseio.PWMOut(LED_RED, duty_cycle=65535)
@@ -14,7 +15,8 @@ class builtin_led:
         self.pwm_led_blue = pulseio.PWMOut(LED_BLUE, duty_cycle=65535)
     
     def set_rgb(self, red, green, blue):
-        """set_rgb(red, green, blue); Values must be between 0-255"""
+        """set_rgb(red, green, blue)
+        Values must be between 0-255"""
         try:
             self.pwm_led_red.duty_cycle = (255 - red) * 257 if red < 256 else 0
             self.pwm_led_green.duty_cycle = (255 - green) * 257 if green < 256 else 0
